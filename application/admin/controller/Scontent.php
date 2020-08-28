dbe6b8d9fe402580c49981cb612b1bfd630edf45
asd623623am <13652052151@163.com>
2020-08-28 11:52:11 +0800

    bingwoo:添加打包费

--- application/admin/controller/Scontent.php
+++ application/admin/controller/Scontent.php
@@ -10,27 +10,28 @@ class Scontent extends Common
 {
     public function sadd()
     {
+        $admin = \session('admin');
+        $where = [
+            'storeid'   => $admin['storeid']
+        ];
         if (request()->isPost() && request()->isAjax()) {
             $data = input();
-            $admin = \session('admin');
-            $where = [
-                'storeid'   => $admin['storeid']
-            ];
-
             $res = Db::table('xm_store_content')->where($where)->find();
             if($res == null){
-
-
                 if($data['banner_url'] != ''){
                     $data['banner_url'] = $str= ltrim ($data['banner_url'],'#');
                 }
-
                 $update = [
                     'storeid'  => $admin['storeid'],
                     'img'       => $data['banner_url'],
+                    'packing_fee'   => $data['packing_fee'],
                     'content'   => $data['content'],
                     'ctime'     => time()
                 ];
+                $arr['is_charge'] = 0;
+                if(isset($data['is_charge'])){
+                    $update['is_charge'] = 1;
+                }
                 $info=Db::table('xm_store_content')->insertGetId($update);
                 if($info){
                     $this -> addLog('添加了店铺简介');
@@ -39,15 +40,19 @@ class Scontent extends Common
                     fail('添加失败');
                 }
             } else {
-
                 if($data['banner_url'] != ''){
                     $data['banner_url'] = $str= ltrim ($data['banner_url'],'#');
                 }
                 $arr = [
                     'img'       => $data['banner_url'],
+                    'packing_fee'   => $data['packing_fee'],
                     'content'   => $data['content'],
                     'utime'     => time()
                 ];
+                $arr['is_charge'] = 0;
+                if(isset($data['is_charge'])){
+                    $arr['is_charge'] = 1;
+                }
                 $info = Db::table('xm_store_content')->where($where)->setField($arr);
                 if($info){
                     $this -> addLog('修改了店铺简介');
@@ -57,6 +62,8 @@ class Scontent extends Common
                 }
             }
         } else {
+            $storeInfo = Db::table('xm_store_content')->where($where)->find();
+            $this->assign('storeInfo',$storeInfo);
             return view();
         }
     }
