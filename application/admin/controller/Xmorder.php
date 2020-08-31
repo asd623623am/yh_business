8624e4cddff9ed44316329d32e31754146a6153b
jiadongjie <862095309@qq.com>
2020-08-31 11:00:34 +0800

    退款

--- application/admin/controller/Xmorder.php
+++ application/admin/controller/Xmorder.php
@@ -313,7 +313,7 @@ class Xmorder extends Common
     			$sign = $this->appgetSign($signdata,$secret);
     			$arr['signValue']=strtoupper($sign);
     			$url = "http://yhyr.com.cn/YinHeLoan/yinHe/refundWmpPay.action";
-				$res = request_post($url,$arr);
+				$res = $this->sendpostss($url,$arr);
 				dump($res);exit;
     			if ($res['returnCode'] == 0000) {
     				$where = [
@@ -351,5 +351,32 @@ class Xmorder extends Common
         // 生成sign
         $sign = md5($params);
         return $sign;
-    }
+	}
+	 /**
+     * 发送post formdata请求
+     */
+	public function sendpostss($url,array $data)
+	{
+	    $data = @json_encode($data);
+
+	    $headers = [
+	        'Content-Type: application/json;charset=utf-8',
+	        'Content-Length: ' . strlen($data)
+	    ];
+
+
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_HEADER, false);
+		curl_setopt($curl, CURLOPT_POST, true);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 8);
+		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+
+		$output = curl_exec($curl);
+		curl_close($curl);
+		return @json_decode($output, true);
+	}
 }
