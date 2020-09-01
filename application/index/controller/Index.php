fe9e572379d732b5c2eff5877642e1a04bc53293
jiadongjie <862095309@qq.com>
2020-09-01 14:02:26 +0800

    111

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1311,32 +1311,27 @@ class Index extends Controller
 	{
 
 		$data = input();
-		// file_put_contents('./a3.log',\json_encode($data));
-		// if($_SERVER['REQUEST_METHOD'] == 'GET'){
-			
-			
-			// $token = 'M16kxQCL9KGNyOU5';
-			// $array = array( $token, $data['timestamp'], $data['nonce']);
-			// sort($array, SORT_STRING);
-			// $str = implode($array);
-			// $ress = sha1($str);
-			// if( $ress == $data['signature'] ){
-			// 	file_put_contents('./a3.log','4567');
-			// 	echo $data['echostr'];exit;
-			// }else{
-			// 	return null;
-			// }
-		
-		
-		$this->responseMsg();
-		
+		file_put_contents('./a3.log',\json_encode($data));
+		if($_SERVER['REQUEST_METHOD'] == 'GET'){
+			$token = 'M16kxQCL9KGNyOU5';
+			$array = array( $token, $data['timestamp'], $data['nonce']);
+			sort($array, SORT_STRING);
+			$str = implode($array);
+			$ress = sha1($str);
+			if( $ress == $data['signature'] ){
+				file_put_contents('./a3.log','4567');
+				echo $data['echostr'];exit;
+			}else{
+				return null;
+			}
 			// 	exit;
-		// } else {
+		} else {
+		$this->responseMsg();
 		// 	file_put_contents('./a3.log','789');
 		// 	$url = 'https://possji.com:8088/yinheorder/wxpublic/verifytoken';
 		// 	$this->sendpostss($url,$data);
 		// 	exit;
-		// }
+		}
 	}
 
 	public function valid()
