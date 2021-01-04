043e11f9ab86863e66aadc7cd9081765666d0267
jiadongjie <862095309@qq.com>
2021-01-04 10:55:43 +0800

    冻结你的心提交退款

--- application/admin/controller/Xmorder.php
+++ application/admin/controller/Xmorder.php
@@ -383,7 +383,8 @@ class Xmorder extends Common
             $secret = $app[0]['mini_appsecret'];
             $termNo = $app[0]['termNo'];
             $merId = $app[0]['merId'];
-
+			$return_fee = $result['pay_fee'];
+            $result['pay_fee']=sprintf("%1\$.2f", $result['pay_fee']-$result['refund_fee']);
             $result['pay_fee']=str_replace('.', '', $result['pay_fee']);
             $lens = strlen($result['pay_fee']);
             // $data['deposit_money']=sprintf("%012d", $data);//生成12位数，不足前面补0
@@ -425,7 +426,7 @@ class Xmorder extends Common
                 ];
                 $newData = [
 					'pay_status'	=> 3,
-					'refund_fee'	=> $result['pay_fee']
+					'refund_fee'	=> $return_fee
                 ];
                 $infos = model('Xmorder')->where($wheres)->setField($newData);
                 if ($infos) {
